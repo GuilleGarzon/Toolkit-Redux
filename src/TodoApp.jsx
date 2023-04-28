5be7464e860abc4d7be6f248e69c930705d160a9
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-04-28 17:32:07 -0500

    feat: Get todo By id

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,8 +1,20 @@
-import { useGetTodosQuery } from './store/Apis/todosApi';
+import { useState } from 'react';
+import { useGetTodosQuery, useGetTodoByIdQuery } from './store/Apis/todosApi';
 
 export const TodoApp = () => {
 
-  const { data: todos = [], isLoading } = useGetTodosQuery();
+  const [ todoId, setTodoId ] = useState(1);
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoByIdQuery( todoId );
+
+  const nextTodo = () => {
+    setTodoId( todoId + 1 );
+  }
+
+  const prevTodo = () => {
+    if ( todoId === 1 ) return;
+    setTodoId( todoId - 1 );
+  }
 
   return (
     <>
@@ -10,19 +22,22 @@ export const TodoApp = () => {
       <hr />
       <h4>isLoading: { isLoading ? 'True': 'False' } </h4>
 
-      <pre>...</pre>
-      
-      <ul>
+      <pre>{ JSON.stringify(todo) }</pre>
+
+      <button onClick={ prevTodo }>
+        Prev Todo
+      </button>      
+      <button onClick={ nextTodo }>
+        Next Todo
+      </button>
+      {/* <ul>
         { todos.map( todo => (
           <li key={ todo.id }>
             <strong> { todo.completed ? 'Done' : 'Pending' } </strong>{ todo.title }
           </li>
         )) }
-      </ul>
+      </ul> */}
 
-      <button>
-        Next Todo
-      </button>
     </>
   )
 }
